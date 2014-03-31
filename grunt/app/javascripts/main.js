6087a8fbbeda386d6e533f748f9a72e6881d0120
Guilherme Henrique <guih@hotmail.com.br>
2014-03-31 02:24:52 -0300

    UI, ui improvements ux

--- grunt/app/javascripts/main.js
+++ grunt/app/javascripts/main.js
@@ -1,1 +1,14 @@
-var MusicPlayer = {};  
+var MusicPlayer = function(){
+	this.init();
+};
+MusicPlayer.prototype.init = function() {
+	this.handleTracksBox();
+};
+MusicPlayer.prototype.handleTracksBox = function(){
+	jQuery('.add-media').on('click', function(){
+		jQuery('#playlist').slideToggle();
+	});
+};
+window.onload=function(){
+	return ( new MusicPlayer() );
+};
