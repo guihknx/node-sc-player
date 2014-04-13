b32231dfcffc4b7eab1b43e971da4718b9d995ff
Guilherme Henrique <guilherme.henrique@apiki.com>
2014-04-13 15:28:48 -0300

    Implementation of volume control

--- grunt/app/javascripts/main.js
+++ grunt/app/javascripts/main.js
@@ -2,13 +2,63 @@ var MusicPlayer = function(){
 	this.init();
 };
 MusicPlayer.prototype.init = function() {
-	this.handleTracksBox();
+	this.handlevolsBox();
+	this.handleVolume();
 };
-MusicPlayer.prototype.handleTracksBox = function(){
+MusicPlayer.prototype.handlevolsBox = function(){
 	jQuery('.add-media').on('click', function(){
 		jQuery('#playlist').slideToggle();
 	});
 };
+MusicPlayer.prototype.handleVolume = function(){
+var isDragging = false, startClientY,startThumbY;
+
+$('.controller')
+    .mousedown(function(event){
+        isDragging = true;
+        startClientY = event.clientY;
+        startThumbY = parseInt($(this).css('top'), null);
+        return false; 
+    });
+
+$('.vol')
+    .mousedown(function(event){
+        isDragging = true;
+        startClientY = event.clientY;
+        startThumbY = event.clientY - $('.vol').offset().top - $('.controller').height()/2;
+        $('.controller').css('top', startThumbY +"px");
+        return false; 
+    });
+
+$(window)
+    .mousemove(function(event){
+        if (isDragging)
+        {
+            var thumbY = startThumbY + event.clientY - startClientY;
+            var volHeight = $('.vol').height() - $('.controller').height();
+            
+            if (thumbY > volHeight){
+                thumbY = volHeight;
+            }
+            else if (thumbY < 0){
+                thumbY = 0;
+            }
+            
+            var percentage = Math.floor((volHeight - thumbY)/volHeight * 100);
+            
+            $('.controller')
+                .css('top', thumbY +"px")
+                .text(percentage +"%");
+            
+            
+            return false; 
+        }
+    })
+    .mouseup(function(){
+        isDragging = false;
+    });
+};
+
 window.onload=function(){
 	return ( new MusicPlayer() );
 };
