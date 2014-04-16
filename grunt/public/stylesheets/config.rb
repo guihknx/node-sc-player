1e506e1696738cbec0c43deb6150fb9e41d85044
Guilherme Henrique <guih@hotmail.com.br>
2014-04-16 20:30:38 -0300

    Add compressed output style in config.rb file

--- grunt/public/stylesheets/config.rb
+++ grunt/public/stylesheets/config.rb
@@ -1,7 +1,4 @@
 http_path = "/"
 sass_dir = "."
 css_dir = "../"
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compac
-relative_assets= true
+output_style = :compressed
