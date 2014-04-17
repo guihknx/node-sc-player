025ee32b4fee912821bdb74b9b9ca1ca2bfdf1c9
Guilherme Henrique <guih@hotmail.com.br>
2014-04-17 15:08:42 -0300

    Add new route to accep track id as param

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var fetch = require('./routes/fetch.js');
 var http = require('http');
 var jQuery = require("jquery");
 var path = require('path');
-
+var    appDir = path.dirname(require.main.filename);
 var app = express();
+app.set('base', '/');
 
 // all environments
 app.set('port', process.env.PORT || 4000);
@@ -22,18 +23,26 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public/')));
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+app.get(/^\/public\/(.*)/, function (req, res) {
+    console.log('assets/app/static/' + req.params[0]);
+    res.sendfile('assets/app/static/' + req.params[0]);
+});
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/fetch', fetch.fetch);
+app.get(/\/post\/([^\/]+)\/?/,routes.index );
+app.use(/\/post\/([^\/]+)\/?/, express.static(__dirname + '../public'));
+
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log(this);
+  console.log('Express server listening on port ' + app.get('port') );
 });
